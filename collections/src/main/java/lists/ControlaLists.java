06c9c24e91b23071d46b7a1926aa6b8b6c556a1f
LuFelix <luciano@tiweb.app.br>
2023-03-14 12:05:36 -0300

    Lists 4 etapa

--- collections/src/main/java/lists/ControlaLists.java
+++ collections/src/main/java/lists/ControlaLists.java
@@ -1,13 +1,16 @@
 package lists;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ControlaLists {
     public ControlaLists() {
     }
-
     public static void main(String[] args) {
+        //primeiraEtapa();
+        //segundaEtapa();
+        terceiraEtapa();
+
+    }
+    private static void primeiraEtapa(){
         /**
          * Instancia uma Lista de Animais e confere os elementos
          */
@@ -26,7 +29,7 @@ public class ControlaLists {
         /**
          * Exibe uma Lista de carros em seguida exibe uma posiçãoapós, troca um elemento no fim troca um elemnto
          */
-        System.out.println("--\t Exibe uma Lista de carros em seguida exibe uma posiçãoapós, troca um elemento no fim troca um elemnto \t-- ");
+        System.out.println("--\t Exibe uma Lista de carros, exibe uma posição e, após, troca um elemento \t-- ");
         List<String> listaCarros = new ArrayList<>();
         listaCarros.add("Fusca");
         listaCarros.add("Kombi");
@@ -42,22 +45,98 @@ public class ControlaLists {
         System.out.println("Terceira posição: " + listaCarros.get(2));
     }
 
-    private static void mediaNotas() {
-        System.out.println("--\t Média de Notas \t--");
-        double aux = 0.0;
-        List<Double> notas = new ArrayList<>();
+    /**
+     * Receber notas, armazenar em uma lista, mostrar a menor,
+     * maior e média usando Collections Interface
+     */
+    private static void segundaEtapa() {
+        System.out.println("--\t Segunda Etapa \t--");
+        List<Double> listaNotas = new ArrayList<>();
         Scanner in = new Scanner(System.in);
 
-        for (int i = 0; i < 6; ++i) {
+        for (int i = 0; i < 3; ++i) {
             System.out.println("Insira uma nota: ");
-            notas.add(in.nextDouble());
+            listaNotas.add(in.nextDouble());
         }
 
-        for (int i = 0; i < notas.size(); i++) {
-            aux = aux + notas.get(i);
+        System.out.println();
+        System.out.println("--\t Menor das Notas \t--");
+        System.out.println("Nota: " + Collections.min(listaNotas));
+        System.out.println();
+        System.out.println("--\t Maior das Notas \t--");
+        System.out.println("Notas: " + Collections.max(listaNotas));
+        System.out.println();
+        System.out.println("--\t Média das Notas \t--");
+        double aux = 0.0;
+        for (int i = 0; i < listaNotas.size(); i++) {
+            aux = aux + listaNotas.get(i);
         }
+        aux /= listaNotas.size();
+        System.out.printf("Média das Notas: %.2f" , aux);
 
-        aux /= (double) notas.size();
-        System.out.println("Media de Notas: " + aux);
+        System.out.println("\n");
+        System.out.println("--\t Fim da execução da segunda etapa \t--");
+        System.out.println();
+    }
+
+    /**
+     * Receber uma lista de notas, iterar, remover elemento por valor,
+     * remover elemento pelo indice
+     */
+    private static void terceiraEtapa(){
+        System.out.println("--\t Terceira Etapa \t--");
+        System.out.println();
+        System.out.println("--\t Iterar sobre a Lista");
+
+        List<Double> listaNotas = new ArrayList<Double>(List.of(7.7,8.9,4.5,9.9,6.7,0d));
+        Iterator<Double> iterator = listaNotas.iterator();
+        Double soma = 0.0;
+        while (iterator.hasNext()){
+            soma = soma + iterator.next();
+        }
+        System.out.println(listaNotas.toString());
+
+        System.out.println();
+        System.out.printf("Exibe a soma %.2f", soma);
+        System.out.println();
+
+        System.out.println();
+        System.out.println("--\t Remove a primeira nota");
+        listaNotas.remove(0);
+        System.out.println(listaNotas);
+
+        System.out.println();
+        System.out.println("--\t Remove a nota 0");
+        listaNotas.remove(0.d);
+        System.out.println(listaNotas);
+
+        System.out.println();
+        System.out.println("--\t Remove as notas menores que 7");
+
+        Iterator<Double> iterator1 = listaNotas.iterator();
+        while (iterator1.hasNext()){
+            if (iterator1.next()<7d){
+                iterator1.remove();
+            }
+        }
+        System.out.println(listaNotas);
+        System.out.println();
+
+        System.out.println("--\t Apagar a lista");
+        listaNotas.clear();
+        System.out.println("--\t Lista Atual ");
+        System.out.println(listaNotas);
+
+        System.out.println();
+        System.out.println("--\t A lista está vazia? " +
+                (listaNotas.isEmpty()?"Sim":"Nâo"));
+
+        System.out.println("\n");
+        System.out.println("--\t Fim da execução da terceira etapa \t--");
+        System.out.println();
+    }
+
+    public static void quartaEtapa (){
+    
     }
 }
